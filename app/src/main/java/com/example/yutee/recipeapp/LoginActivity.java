969ce448de5009a1e540b83e2697885867528d6c
Yublue <etim.utibe@gmail.com>
2019-01-19 23:05:55 +0100

    I just implemented the login activity

--- app/src/main/java/com/example/yutee/recipeapp/LoginActivity.java
+++ app/src/main/java/com/example/yutee/recipeapp/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.example.yutee.recipeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +25,51 @@ public class LoginActivity extends AppCompatActivity {
         loginmail = (EditText)findViewById(R.id.loginMail);
         loginPass = (EditText)findViewById(R.id.loginPassword);
         loginBtn = (Button) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogIn();
+
+            }
+        });
     }
 
+    public void doLogIn(){
+        if(!validate()) {
+            Toast.makeText(this, "mistake", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+
+    }
+
+    public boolean validate() {
+
+        String inputMail = loginmail.getText().toString();
+        String inPutPassWord = loginPass.getText().toString();
+
+
+        if (inputMail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(inputMail).matches()) {
+
+            loginmail.setError("Invalid email");
+            return false;
+        }
+
+        if (inPutPassWord.isEmpty() || inPutPassWord.length() < 4 || inPutPassWord.equals(" ")) {
+
+            loginPass.setError("It has to be alphanumeric character");
+            return false;
+        }
+
+        return true;
+
+
+    }
+
+
 }
+
+
